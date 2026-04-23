e3ea6525be56e8672fb95f1bf5cd2c97a60aa651
Xinyu Wang <45314412+Matheritasiv@users.noreply.github.com>
2026-04-23 17:02:32 +0800

    Add files via upload

--- wrap.c
+++ wrap.c
@@ -1,59 +1,18 @@
-#include<assert.h>
-#include<pthread.h>
-#include<unistd.h>
+#include<stddef.h>
 #include"scheme.h"
 
 extern const unsigned char petite_content[] __asm__ ("_binary_petite_boot_start");
 extern const unsigned char petite_end[]     __asm__ ("_binary_petite_boot_end");
 int init_state = 0;
 
-typedef struct targs {
-	int pipe_des;
-	const unsigned char *content;
-	unsigned int length;
-} TARGS;
-
-static void *data_writer(void *targs)
-{
-	int pipe_des                 = ((TARGS *)targs)->pipe_des;
-	const unsigned char *content = ((TARGS *)targs)->content;
-	unsigned int length          = ((TARGS *)targs)->length;
-	assert(write(pipe_des, content, length) == length);
-	close(pipe_des);
-	return NULL;
-}
-
 extern int petite_init(const unsigned char *boot_content, unsigned int boot_len, void (*custom_init)(void))
 {
-	pthread_t pid_petite, pid_boot;
 	if (init_state) return -1;
 	init_state = 1;
 	Sscheme_init(NULL);
-	{
-		int pipe_des[2];
-		TARGS targs = {
-			.content = petite_content,
-			.length  = petite_end - petite_content
-		};
-		assert(pipe(pipe_des) == 0);
-		targs.pipe_des = pipe_des[1];
-		assert(pthread_create(&pid_petite, NULL, data_writer, &targs) == 0);
-		Sregister_boot_file_fd("petite", pipe_des[0]);
-	}
-	{
-		int pipe_des[2];
-		TARGS targs = {
-			.content = boot_content,
-			.length  = boot_len
-		};
-		assert(pipe(pipe_des) == 0);
-		targs.pipe_des = pipe_des[1];
-		assert(pthread_create(&pid_boot, NULL, data_writer, &targs) == 0);
-		Sregister_boot_file_fd("app", pipe_des[0]);
-	}
+	Sregister_boot_file_bytes("petite", (void *)petite_content, petite_end - petite_content);
+	Sregister_boot_file_bytes("app", (void *)boot_content, boot_len);
 	Sbuild_heap(NULL, custom_init);
-	pthread_join(pid_petite, NULL);
-	pthread_join(pid_boot, NULL);
 	return 0;
 }
 
